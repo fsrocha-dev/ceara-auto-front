5b76317ba770583b4c003e18278f1ab9c06e6583
fsrocha-dev <contato.frankrocha@gmail.com>
2019-03-01 13:56:47 -0300

    Criaçao dos componentes principais

---
+++ src/components/template/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="header">
+        Header
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+    .header {
+        grid-area: header;
+        background: blue;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
