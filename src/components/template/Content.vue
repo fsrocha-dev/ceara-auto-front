2c3771ec762060667d5ba74b74e9bed1ba88947a
fsrocha-dev <contato.frankrocha@gmail.com>
2019-03-02 23:28:18 -0300

    Alteraçoes no content

--- src/components/template/Content.vue
+++ src/components/template/Content.vue
@@ -1,7 +1,65 @@
 <template>
-    <div class="content">
-
-    </div>
+    <section class="content">
+        <div class="container">
+            <div class="row pt-3">
+                <div class="col-12">
+                    <h3>Últimos adicionados</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-9">
+                    <div class="row">
+                        <div class="col-lg-4 col-sm-12 mt-4">
+                            <div class="card" style="width: 16rem;">
+                                <img class="card-img-top" src="../../assets/cars/hilux.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title small">HILUX 2.7 SRV 4X2 CD 16V FLEX 4P AUTOMÁTICO 2014</h5>
+                                    <span class="lead text-primary font-weight-bold">R$ 83.900</span>
+                                    <p class="card-text small">
+                                        Completo , automático, couro , capota, protetor , acessórios, segundo dono Toyota 2014, Automático, 61km+
+                                    </p>
+                                    <a href="#" class="btn btn-primary btn-block">Ver detalhes</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-12 mt-4">
+                            <div class="card" style="width: 16rem;">
+                                <img class="card-img-top" src="../../assets/cars/hondaFit.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title small">
+                                        FIT 1.5 LX 16V FLEX 4P AUTOMÁTICO 2014                
+                                    </h5>
+                                    <span class="lead text-primary font-weight-bold">R$ 52.900</span>
+                                    <p class="card-text small">
+                                        Completo , automático, segundo dono, revisado, ótimo para passeio
+                                    </p>
+                                    <a href="#" class="btn btn-primary btn-block">Ver detalhes</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-12 mt-4">
+                            <div class="card" style="width: 16rem;">
+                                <img class="card-img-top" src="../../assets/cars/captiva.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title small">
+                                        CAPTIVA SPORT 3.0 SFI AWD V6 24V GASOLINA 4P AUTOMÁTICO 2011 
+                                    </h5>
+                                    <span class="lead text-primary font-weight-bold">R$ 43.900</span>
+                                    <p class="card-text small">
+                                        Top de linha , automático, couro, segundo dono, Automático
+                                    </p>
+                                    <a href="#" class="btn btn-primary btn-block">Ver detalhes</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-12 mt-4 text-center" style="background: #E7E7E7; padding-top: 15%; color: #ADADAD;">
+                    ANUNCIE AQUI
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
